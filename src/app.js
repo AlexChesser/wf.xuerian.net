a8325ffc127fc43d46c9012df1296203a358cf1d
Alex Chesser <achesser@klick.com>
2018-02-09 09:49:44 -0500

    fetch-fix

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 "use strict"
-const jsonRoot = "/wf.xuerian.net/json";
+const jsonRoot = "/wf.xuerian.net/json/";
 
 // We use more than Fetch, but it's an easy way to catch browsers that definitely won't work and get the attention of users who won't read the Welcome page before posting about the page not working
 if (typeof fetch === "undefined") {
